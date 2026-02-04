a2dfae3621dd743fe4f237911a61055c4c96cf19
Suneetha Porumamilla <suneetha.porumamilla@MacBook-Air-3.local>
2026-02-04 16:08:29 -0500

    Changes

--- 1_basic_agent.py
+++ 1_basic_agent.py
@@ -1,16 +1,40 @@
+import os
+
+from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
 
 def get_weather(city: str) -> str:
     """Get weather for a given city."""
     return f"It's always sunny in {city}!"
 
+
+model = init_chat_model(
+    "gpt-4.1",
+    api_key=os.getenv(('OPENAI_API_KEY'))
+)
+
 agent = create_agent(
-    model="claude-sonnet-4-5-20250929",
+    model=model,
     tools=[get_weather],
     system_prompt="You are a helpful assistant",
 )
 
 # Run the agent
-agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+result = agent.invoke({"messages": [
+    {
+        "role": "user",
+        "content": "what is the weather in sf"
+    }
+]}
 )
+
+# 2. Extract the content from the last message
+# The messages key contains the full history of the turn
+final_message = result["messages"][-1]
+
+# 3. Print the text content
+print(final_message.content)
--- 2_real_world_agent.py
+++ 2_real_world_agent.py
@@ -1,6 +1,55 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
 from langchain.agents.structured_output import ToolStrategy
 
 from dataclasses import dataclass
+from langchain.tools import tool, ToolRuntime
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import InMemorySaver
+
+load_dotenv()
+
+SYSTEM_PROMPT = """You are an expert weather forecaster, who speaks in puns.
+
+You have access to two tools:
+
+- get_weather_for_location: use this to get the weather for a specific location
+- get_user_location: use this to get the user's location
+
+If a user asks you for the weather, make sure you know the location. If you can tell from the 
+question that they mean wherever they are, use the get_user_location tool to find their 
+location."""
+@tool
+def get_weather_for_location(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+@dataclass
+class Context:
+    """Custom runtime context schema."""
+    user_id: str
+
+@tool
+def get_user_location(runtime: ToolRuntime[Context]) -> str:
+    """Retrieve user information based on user ID."""
+    user_id = runtime.context.user_id
+    return "Florida" if user_id == "1" else "SF"
+
+
+
+model = init_chat_model(
+    "gpt-4.1",
+    temperature=0.5,
+    timeout=10,
+    max_tokens=1000,
+    api_key= os.getenv(('OPENAI_API_KEY'))
+)
+
+
+
+checkpointer = InMemorySaver()
 
 # We use a dataclass here, but Pydantic models are also supported.
 @dataclass
@@ -19,6 +68,8 @@ agent = create_agent(
     response_format=ToolStrategy(ResponseFormat),
     checkpointer=checkpointer
 )
+
+
 
 # `thread_id` is a unique identifier for a given conversation.
 config = {"configurable": {"thread_id": "1"}}
